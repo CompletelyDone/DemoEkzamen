8df147db3a31cdf50014b0071bab7c05d043e8df
Roman <roman.songoku.army@gmail.com>
2023-11-30 20:18:41 +0700

    asd

---
+++ SF2022User13Lib.dll/Calculations.cs
@@ -0,0 +1,10 @@
+﻿namespace SF2022User13Lib.dll
+{
+    public class Calculations
+    {
+        public string[] AvailablePeriods(TimeSpan[] startTimes,int[] durations,TimeSpan beginWorkingTime,TimeSpan endWorkingTime,int consultationTime)
+        {
+            return new string[0];
+        }
+    }
+}
---
+++ РусскаяКосметика.Тесты/UnitTest.cs
@@ -0,0 +1,18 @@
+using SF2022User13Lib;
+
+namespace РусскаяКосметика.Тесты
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase()]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
